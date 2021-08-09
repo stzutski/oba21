aba76832ac09eac881b16926bf6c2ab6ea9dac0c
root <roberto.stzutski.com>
2021-08-09 17:27:34 -0300

    Configuracao inicial do Framework Slim para rotas do sistema

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 	<?php 
-	include_once('php/template/includes/header.tpl.php');
-	include_once('php/template/includes/top-menu.tpl.php');
-  include_once('php/template/includes/footer.tpl.php');
+  require_once('vendor/autoload.php');
+
+  $_page='';
+
+  use \Slim\Slim;
+  $app = new \Slim\Slim();  
+  
+  $app->get('/', function () {
+    //echo 'inicial';
+    
+    include_once('php/template/includes/header.tpl.php');
+    include_once('php/template/includes/top-menu.tpl.php');
+    include_once('php/template/includes/footer.tpl.php');
+    
+  });  
+    
+  
+  $app->get('/cadastro', function () {
+    
+    
+    include_once('php/template/includes/header.tpl.php');
+    //include_once('php/template/includes/top-menu.tpl.php');
+    
+    echo 'cadastro';
+    
+    include_once('php/template/includes/footer.tpl.php');
+    
+  });     
+    
+  $app->run();  
+    
 	?>   
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2537265ec798d9fed301d1453762a5685a892263',
+        'name' => 'actoweb/oba21',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'actoweb/oba21' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2537265ec798d9fed301d1453762a5685a892263',
+            'dev_requirement' => false,
+        ),
+        'slim/slim' => array(
+            'pretty_version' => '2.0.0',
+            'version' => '2.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../slim/slim',
+            'aliases' => array(),
+            'reference' => '3e95c9abbc57a8f023ab4dacebcab9dae4d9f1f0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
