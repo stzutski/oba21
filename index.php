2fb105ae5b0c63c9d4bda87bed5e26d1aa6e6d5d
root <roberto.stzutski.com>
2021-08-09 17:05:47 -0300

    carga de arquivos iniciais

---
+++ index.php
@@ -0,0 +1,5 @@
+	<?php 
+	include_once('php/template/includes/header.tpl.php');
+	include_once('php/template/includes/top-menu.tpl.php');
+  include_once('php/template/includes/footer.tpl.php');
+	?>   
---
+++ php/template/includes/footer.tpl.php
@@ -0,0 +1,35 @@
+
+    <script>
+    //var pathApp = '<?php // echo $_app_path;?>';
+    </script>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="assets/js/jquery-3.5.1.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/jquery.resizableColumns.min.js"></script>
+    <script src="assets/js/bootstrap-table.min.js"></script>
+    <script src="assets/js/tableExport.min.js"></script>
+    <script src="assets/js/jspdf.min.js"></script>
+    <script src="assets/js/jspdf.plugin.autotable.js"></script>
+    <script src="assets/js/bootstrap-table-export.min.js"></script>
+    <script src="assets/js/bootstrap-table-mobile.min.js"></script>
+    <script src="assets/js/tableExport.min.js"></script>
+    <script src="assets/js/bootstrap-table-export.min.js"></script>
+    <script src="assets/js/bootstrap-table-copy-rows.min.js"></script>
+    <script src="assets/js/bootstrap-table-reorder-rows.min.js"></script>
+    <script src="assets/js/bootstrap-table-resizable.min.js"></script>
+    <script src="assets/js/bootstrap-table-pt-BR.js"></script>
+    <script src="assets/js/bootstrap-table-sticky-header.min.js"></script>
+    <script src="assets/js/alertify.min.js"></script>
+    <script src="assets/fancybox/jquery.fancybox.pack.js"></script>
+    <script src="assets/js/scripts.js?ver=<?php echo time();?>"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
