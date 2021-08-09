2537265ec798d9fed301d1453762a5685a892263
root <roberto.stzutski.com>
2021-08-09 17:18:26 -0300

    Ajustes no Menu do Topo

--- php/template/includes/top-menu.tpl.php
+++ php/template/includes/top-menu.tpl.php
@@ -4,61 +4,54 @@
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarCollapse">
-    <ul class="navbar-nav mr-auto">
 
-      <li class="nav-item">
-        <a class="nav-link" href="#">Home</a>
-      </li>
+  <ul class="navbar-nav mr-auto">
+    
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
 
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownL" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-users"></i> Usuários
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownL">
+              <a class="dropdown-item" href="login">Login</a>
+              <a class="dropdown-item" href="login">Rec Senha</a>
+              <a class="dropdown-item" href="login">Senha atualizada</a>
+              <a class="dropdown-item" href="login">Cadastro</a>
+              <a class="dropdown-item" href="login">Confirme seu email</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="">DashBoard</a>
+              <a class="dropdown-item" href="">Escolha o Servicos</a>
+              <a class="dropdown-item" href="">Configurar Servico</a>
+              <a class="dropdown-item" href="">Dados de Cobrança</a>
+              <a class="dropdown-item" href="">Gerar Cobrança</a>
+              <a class="dropdown-item" href="">Form Perfil (auto-save)</a>
+              <a class="dropdown-item" href="">Upload IRPF</a>
+              <a class="dropdown-item" href="">Meus Arquivos</a>
+            </div>
+        </li>
 
-      <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownL" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="fas fa-users"></i> Usuários
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownL">
-            <a class="dropdown-item" href="login">Login</a>
-            <a class="dropdown-item" href="login">Rec Senha</a>
-            <a class="dropdown-item" href="login">Senha atualizada</a>
-            <a class="dropdown-item" href="login">Cadastro</a>
-            <a class="dropdown-item" href="login">Confirme seu email</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="">DashBoard</a>
-            <a class="dropdown-item" href="">Escolha o Servicos</a>
-            <a class="dropdown-item" href="">Configurar Servico</a>
-            <a class="dropdown-item" href="">Dados de Cobrança</a>
-            <a class="dropdown-item" href="">Gerar Cobrança</a>
-            <a class="dropdown-item" href="">Form Perfil (auto-save)</a>
-            <a class="dropdown-item" href="">Upload IRPF</a>
-            <a class="dropdown-item" href="">Meus Arquivos</a>
-          </div>
-          
-      </li>
-
-      <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownL" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="fas fa-user"></i> Admin
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownL">
-            <a class="dropdown-item" href="">DashBoard</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="login">Cadastros</a>
-            <a class="dropdown-item" href="login">Rec Senha</a>
-            <a class="dropdown-item" href="login">Senha atualizada</a>
-            <a class="dropdown-item" href="login">Cadastro</a>
-            <a class="dropdown-item" href="login">Confirme seu email</a>
-
-
-          </div>
-          
-      </li>
-
-    </ul>
-
-<div class="pull-right" >
-     
-     
-<ul class="navbar-nav ml-auto">
-
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownR" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-user"></i> Admin
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownR">
+              <a class="dropdown-item" href="">DashBoard</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="login">Cadastros</a>
+              <a class="dropdown-item" href="login">Rec Senha</a>
+              <a class="dropdown-item" href="login">Senha atualizada</a>
+              <a class="dropdown-item" href="login">Cadastro</a>
+              <a class="dropdown-item" href="login">Confirme seu email</a>
+            </div>
+        </li>
+    
+  </ul>
+  </div>
+    <div class="pull-right" >
+        <ul class="navbar-nav ml-auto">
             <li class="nav-item dropdown">
                 <a class="nav-link" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   <b>
@@ -71,6 +64,5 @@
                 </div>
             </li>
         </ul>
-
-</div>
+    </div>
 </nav>
