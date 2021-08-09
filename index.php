d5c111e5ceb93f5d836fdb5b6bf8a5918cf46d91
root <roberto.stzutski.com>
2021-08-09 17:02:30 -0300

    limpeza arquivos iniciais

--- index.php
+++
@@ -1,5 +0,0 @@
-	<?php 
-	include_once('php/template/includes/header.tpl.php');
-	include_once('php/template/includes/top-menu.tpl.php');
-  include_once('php/template/includes/footer.tpl.php');
-	?>   
